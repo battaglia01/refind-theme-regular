55b44aeecda9773baac57b5e62143d203058b8c6
Mike Battaglia <battaglia01@gmail.com>
2018-08-13 01:46:28 -0400

    Added new background, install script, tweaked theme.conf

---
+++ install-theme.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#install theme according to instructions
+sudo rm -rf /boot/efi/EFI/refind/refind-theme-regular 2> /dev/null                              #remove old theme if present
+sudo cp -r . /boot/efi/EFI/refind/refind-theme-regular                                          #copy theme to rEFInd folder
+sudo rm -rf /boot/efi/EFI/refind/refind-theme-regular/src                                       #remove src and .git folders from theme
+sudo rm -rf /boot/efi/EFI/refind/refind-theme-regular/.git
+echo "rEFInd installed! Now add the following two lines to the end of /boot/efi/EFI/refind/refind.conf:"
+echo "include refind-theme-regular/theme.conf"
+echo "default_selection +"
+echo ""
+echo "(Be careful that default_selection isn't already defined elsewhere in the file)"
